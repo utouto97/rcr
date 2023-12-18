b2635b2e5f49ace7d73d27e99da6da657a28a366
utouto97 <utouto97@gmail.com>
2023-12-18 15:38:05 +0900

    ステップ3: tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
 use std::env;
 use std::process;
 
+#[derive(Debug)]
+enum Token {
+    Number(i64),
+    Operator(String),
+}
+
+fn tokenize(s: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut chars = s.chars().peekable();
+    while let Some(c) = chars.next() {
+        match c {
+            '+' | '-' => {
+                tokens.push(Token::Operator(c.to_string()));
+            }
+            '0'..='9' => {
+                let mut num = c.to_digit(10).unwrap() as i64;
+                while let Some('0'..='9') = chars.peek() {
+                    num = num * 10 + chars.next().unwrap().to_digit(10).unwrap() as i64;
+                }
+                tokens.push(Token::Number(num));
+            }
+            ' ' | '\r' | '\n' | '\t' => {}
+            _ => {
+                eprintln!("不正な文字です: {}", c);
+                process::exit(1);
+            }
+        }
+    }
+    tokens
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -8,34 +39,53 @@ fn main() {
         process::exit(1);
     }
 
-    println!(".globl main");
-    println!("main:");
-    println!("  addi a0, zero, 0");
-
-    let mut num: i32 = 0;
-    let mut op = '+';
-    for c in args[1].chars() {
-        match c {
-            '+' => {
-                println!("  addi a0, a0, {}{}", op, num);
-                num = 0;
-                op = '+';
-            }
-            '-' => {
-                println!("  addi a0, a0, {}{}", op, num);
-                num = 0;
-                op = '-';
-            }
-            '0'..='9' => {
-                num = num * 10 + c.to_digit(10).unwrap() as i32;
-            }
-            _ => {
-                eprintln!("不正な文字です: {}", c);
-                process::exit(1);
-            }
+    let tokens = tokenize(&args[1]);
+    if tokens.len() == 0 {
+        eprintln!("式が空です");
+        process::exit(1);
+    }
+    match tokens[0] {
+        Token::Number(n) => {
+            println!(".globl main");
+            println!("main:");
+            println!("  addi a0, zero, {}", n);
+        }
+        _ => {
+            eprintln!("式の最初は数値ではありません");
+            process::exit(1);
         }
     }
-    println!("  addi a0, a0, {}{}", op, num);
+
+    for i in 1..tokens.len() {
+        let token = &tokens[i];
+        match token {
+            Token::Operator(op) => {
+                if tokens.len() <= i + 1 {
+                    eprintln!("演算子の後に式がありません");
+                    process::exit(1);
+                }
+
+                let next_token = &tokens[i + 1];
+                match next_token {
+                    Token::Operator(_) => {
+                        eprintln!("演算子の後に演算子があります");
+                        process::exit(1);
+                    }
+                    Token::Number(n) => {
+                        if op == "+" {
+                            println!("  addi a0, a0, {}", n);
+                        } else if op == "-" {
+                            println!("  addi a0, a0, -{}", n);
+                        } else {
+                            eprintln!("不正な演算子です: {}", op);
+                            process::exit(1);
+                        }
+                    }
+                }
+            }
+            Token::Number(_n) => {}
+        }
+    }
 
     println!("  ret");
 }
