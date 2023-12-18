0a05b8ebf54ca120c4a8db11d9992592cfad9649
utouto97 <utouto97@gmail.com>
2023-12-18 15:01:45 +0900

    ステップ2

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,32 @@ fn main() {
 
     println!(".globl main");
     println!("main:");
-    println!("  addi a0, zero, {}", args[1]);
+    println!("  addi a0, zero, 0");
+
+    let mut num: i32 = 0;
+    let mut op = '+';
+    for c in args[1].chars() {
+        match c {
+            '+' => {
+                println!("  addi a0, a0, {}{}", op, num);
+                num = 0;
+                op = '+';
+            }
+            '-' => {
+                println!("  addi a0, a0, {}{}", op, num);
+                num = 0;
+                op = '-';
+            }
+            '0'..='9' => {
+                num = num * 10 + c.to_digit(10).unwrap() as i32;
+            }
+            _ => {
+                eprintln!("不正な文字です: {}", c);
+                process::exit(1);
+            }
+        }
+    }
+    println!("  addi a0, a0, {}{}", op, num);
+
     println!("  ret");
 }
