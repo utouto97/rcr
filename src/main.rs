63b0738c399237fabd37db7537c068379b301dfe
utouto97 <utouto97@gmail.com>
2023-12-22 18:06:45 +0900

    add parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
 use std::process;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Token {
     Number(i64),
     Operator(String),
+    EOF,
 }
 
 fn tokenize(s: &str) -> Vec<Token> {
@@ -36,6 +37,126 @@ fn tokenize(s: &str) -> Vec<Token> {
 fn tokenize_error(s: &str, pos: usize) {
     println!("{}", s);
     println!("{}", " ".repeat(pos) + "^");
+}
+
+#[derive(Clone, Debug)]
+enum NodeType {
+    ROOT,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    NUM(i64),
+}
+
+#[derive(Clone, Debug)]
+struct Node {
+    value: NodeType,
+    children: Vec<Box<Node>>,
+}
+
+impl Node {
+    fn new(value: NodeType) -> Box<Node> {
+        Box::new(Node {
+            value,
+            children: Vec::new(),
+        })
+    }
+
+    fn add_child(&mut self, child: Box<Node>) -> Box<Node> {
+        Box::new(Node {
+            value: self.value.clone(),
+            children: [self.children.clone(), vec![child]].concat(),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Parser {
+    tokens: Vec<Token>,
+    pos: usize,
+}
+
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Parser {
+        Parser {
+            tokens: [tokens.clone(), vec![Token::EOF]].concat(),
+            pos: 0,
+        }
+    }
+
+    fn token(&mut self) -> &Token {
+        &self.tokens[self.pos]
+    }
+
+    fn next(&mut self) {
+        self.pos += 1;
+    }
+
+    fn parse_expr(&mut self) -> Box<Node> {
+        let mut root = self.parse_mul();
+
+        loop {
+            match self.token() {
+                Token::Operator(op) => match op.as_str() {
+                    "+" => {
+                        self.next();
+                        root = Node::new(NodeType::ADD)
+                            .add_child(root)
+                            .add_child(self.parse_mul())
+                    }
+                    "-" => {
+                        self.next();
+                        root = Node::new(NodeType::SUB)
+                            .add_child(root)
+                            .add_child(self.parse_mul())
+                    }
+                    _ => break,
+                },
+                _ => break,
+            }
+        }
+
+        root
+    }
+
+    fn parse_mul(&mut self) -> Box<Node> {
+        let mut root = self.parse_primary();
+        self.next();
+
+        loop {
+            match self.token() {
+                Token::Operator(op) => match op.as_str() {
+                    "*" => {
+                        self.next();
+                        root = Node::new(NodeType::MUL)
+                            .add_child(root)
+                            .add_child(self.parse_primary())
+                    }
+                    "/" => {
+                        self.next();
+                        root = Node::new(NodeType::DIV)
+                            .add_child(root)
+                            .add_child(self.parse_primary())
+                    }
+                    _ => break,
+                },
+                _ => break,
+            }
+        }
+
+        root
+    }
+
+    fn parse_primary(&mut self) -> Box<Node> {
+        match self.token() {
+            Token::Number(n) => Node::new(NodeType::NUM(*n)),
+            _ => {
+                eprintln!("数値ではありません");
+                process::exit(1);
+            }
+        }
+    }
 }
 
 fn main() {
@@ -87,11 +208,17 @@ fn main() {
                             process::exit(1);
                         }
                     }
+                    _ => {}
                 }
             }
             Token::Number(_n) => {}
+            _ => {}
         }
     }
 
     println!("  ret");
+
+    println!("{:?}", tokens);
+    let mut parser = Parser::new(tokens);
+    println!("{:?}", parser.parse_expr());
 }
